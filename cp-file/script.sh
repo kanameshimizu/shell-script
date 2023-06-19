8aa167c660ffaebf9c105016710581dca137d59b
kanameshimizu <kanames911@gmail.com>
2023-06-19 12:29:23 +0900

    copyスクリプト調整

--- cp-file/script.sh
+++ cp-file/script.sh
@@ -1,15 +1,18 @@
+PATH_TO_DOWNLOAD=/Users/kaname.shimizu/Downloads
+PATH_TO_DIST_IMG=/Users/kaname.shimizu/Documents/Projects/lf-landing-page/dist/img
+
 if [ $1 = "sm" ]
 then
-  mkdir /Users/kanameshimizu/Documents/_private/img/seminar/$2
-  cp /Users/kanameshimizu/Downloads/banner.webp /Users/kanameshimizu/Documents/_private/img/seminar/$2/
+  mkdir $PATH_TO_DIST_IMG/legalforce/seminar/$2
+  cp $PATH_TO_DOWNLOAD/banner.webp $PATH_TO_DIST_IMG/legalforce/seminar/$2/
 elif [ $1 = "dl" ]
 then
-  mkdir /Users/kanameshimizu/Documents/_private/img/download/$2
-  cp /Users/kanameshimizu/Downloads/banner.webp /Users/kanameshimizu/Documents/_private/img/download/$2/
+  mkdir $PATH_TO_DIST_IMG/legalforce/download/$2
+  cp $PATH_TO_DOWNLOAD/banner.webp $PATH_TO_DIST_IMG/legalforce/download/$2/
 elif [ $1 = "sp" ]
 then
-  mkdir /Users/kanameshimizu/Documents/_private/img/speaker/$2
-  cp /Users/kanameshimizu/Downloads/portrait.webp /Users/kanameshimizu/Documents/_private/img/speaker/$2/
+  mkdir $PATH_TO_DIST_IMG/legalforce/speaker/$2
+  cp $PATH_TO_DOWNLOAD/portrait.webp $PATH_TO_DIST_IMG/legalforce/speaker/$2
 else
   :
 fi
